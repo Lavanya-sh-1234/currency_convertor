d7663354600feb0d2ce7bc36e9ca425de61b60f7
Lavanya Singh <lavanya.sh7@gmail.com>
2024-07-18 23:57:54 +0530

    remove spaces

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
 const msg = document.querySelector(".msg");
 
-
-
 for (let select of dropdowns) {
   for (currCode in countryList) {
     let newOption = document.createElement("option");
@@ -26,40 +24,30 @@ for (let select of dropdowns) {
   });
 }
 
-
-
 const updateExchangeRate = async () => {
-
-   let amount = document.querySelector(".amount input");
-   let amtVal = amount.value;
-   if(amtVal === '' || amtVal < 1) {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
     amtVal = 1;
     amount.value = "1";
-   }  
-      const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
-  //  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
-   let response = await fetch(URL);
-   let data = await response.json();
-   let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
-  
+  }
+  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+  let data = await response.json();
+  let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
 
- 
-   
-
-   let finalAmount = amtVal * rate;
-   msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`
+  let finalAmount = amtVal * rate;
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
 };
 
 const updateFlag = (element) => {
   let currCode = element.value;
-console.log({currCode})
+  console.log({ currCode });
   let countryCode = countryList[currCode];
-  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`
-  let img = element.parentElement.querySelector("img")
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
   img.src = newSrc;
 };
-
-
 
 btn.addEventListener("click", (evt) => {
   evt.preventDefault();
@@ -69,4 +57,5 @@ btn.addEventListener("click", (evt) => {
 window.addEventListener("load", () => {
   updateExchangeRate();
 });
+
 
